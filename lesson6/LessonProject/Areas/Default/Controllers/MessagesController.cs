341c8276f1e4c0ad98a98ceecaca88f4e074ce7d
RomanDubinin <Roman230394@yandex.ru>
2014-12-30 16:58:48 +0500

    удалеие сообщений + подгрузил бд

--- lesson6/LessonProject/Areas/Default/Controllers/MessagesController.cs
+++ lesson6/LessonProject/Areas/Default/Controllers/MessagesController.cs
@@ -11,6 +11,7 @@ namespace LessonProject.Areas.Default.Controllers
 		//
 		// GET: /Default/Messages/
 
+		//from topics
 		public ActionResult Index(int topicId)
 		{
 			UserContext.CurrentTopic = topicId;
@@ -18,13 +19,18 @@ namespace LessonProject.Areas.Default.Controllers
 			return View(CreatePageConfig());
 		}
 
+		//after adding or deleting message
 		[HttpPost]
-		public ActionResult Index(string statement)
+		public ActionResult Index(string statement, int messageId)
 		{
-			if(!statement.IsEmpty())
+			if(!string.IsNullOrEmpty(statement))
 			{
 				var message = new Message { Statement = statement, User = CurrentUser, TopicId = UserContext.CurrentTopic };
 				Repository.CreateMessage(message);
+			}
+			if(messageId != 0)
+			{
+				Repository.RemoveMessage(messageId);
 			}
 
 			return View(CreatePageConfig());
